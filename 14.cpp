3a121f5e95711876f8096ea118ade708a3d764e9
Nikolai Prozorov <prozorovmykola@gmail.com>
2021-05-06 16:01:26 +0300

    14.cpp var2

--- 14.cpp
+++ 14.cpp
@@ -1,31 +1,64 @@
 #include "std_lib_facilities.h"
 
-struct money_struct
-{
-    string currency;
-    long banknote;
-    int coin;
-};
-
 class Money
 {
 public:
-    Money(string cur, long b, int coin);
-    money_struct get_money() { return m_struct; }
+    Money();
+    Money(string cur, double d);
+    string get_currency() const { return currency; }
+    int get_banknote() const { return banknote; }
+    int get_coin() const { return coin; }
 
 private:
-    long money;
-    money_struct m_struct;
+    string currency;
+    double money;
+    long banknote;
+    int coin;
+    int rounding_coins();
 };
 
-Money::Money(string cur, long b, int coin)
-    : m_struct{cur, b, coin}
+Money::Money()
 {
+    currency = "$";
+    money = 0;
+    banknote = 0;
+    coin = 0;
+}
+
+int Money::rounding_coins()
+{
+    int temp = (money - banknote) * 1000;
+    return (temp%100 >= 5) ? temp/10 + 1 : temp/10;
+}
+
+Money::Money(string cur, double d)
+    : currency{cur}, money{d}
+{
+    banknote = (long)money;
+    coin = rounding_coins();
+}
+
+istream &operator>>(istream &is, Money &m)
+{
+    string currency;
+    double d;
+    char ch1;
+    is >> currency >> d;
+    if (!is)
+        return is;
+    m = Money(currency, d);
+    return is;
+}
+
+ostream &operator<<(ostream &os, const Money &m)
+{
+    return os << m.get_currency() << " " << m.get_banknote() << "." << m.get_coin();
 }
 
 int main()
 {
-    Money m{"$",123,45};
-    money_struct mn = m.get_money();
-    cout << mn.currency << mn.banknote << "." << mn.coin << endl;
+    //Money m{"UAH", 100.91};
+    Money m;
+    cin >> m;
+    cout << m << endl;
 }
